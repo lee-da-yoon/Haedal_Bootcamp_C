fd285cd2185adfcb55dadec09d62f9f187e9ee56
lee-da-yoon <dlekdbs1121@gmail.com>
2026-04-02 16:47:18 +0900

    2588번 곱셈

--- 1차시과제/boj2588.c
+++ 1차시과제/boj2588.c
@@ -1,5 +1,17 @@
 # include <stdio.h>
 int main() {
-    printf("hello");
+    int one;
+    int two;
+    scanf("%d%d", &one, &two);
+
+    int three = one*((two%100)%10);
+    int four = one*((two%100)/10); 
+    int five = one*(two/100);
+    int six = three + 10*four + 100*five;
+
+    printf("%d\n", three);
+    printf("%d\n",four);
+    printf("%d\n", five);
+    printf("%d\n",six);
     return 0;
 }
